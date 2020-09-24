769a19b09dde9c3f1f7a9d45efcde24d4e0664de
Gopika-gs <email@gsgopika00@gmail.com>
2020-09-24 11:40:20 +0530

    contact us page changed

--- phpfiles/contact.php
+++ phpfiles/contact.php
@@ -1,58 +1,120 @@
 <?php
 error_reporting("NOTICE");
 ?>
-
 <!DOCTYPE html>
 <html lang="zxx">
-	</section><!-- Page top Section end -->
+<head>
+	<title>Complaintify</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="loans HTML Template">
+	<meta name="keywords" content="loans, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon"/>
 
-	<!-- Contact Section end -->
-	<section class="contact-section spad">
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap" rel="stylesheet">
+ 
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page top Section end -->
+	<section class="page-top-section set-bg" data-setbg="image/page-top-bg/2.jpg">
+		<div class="container">
+			<h2>Contact us</h2>
+			<nav class="site-breadcrumb">
+				<a class="sb-item" href="#">Home</a>
+				<span class="sb-item active">Contact us</span>
+			</nav>
+		</div>
+	<section class="about-section spad">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-4">
-					
+				<div class="col-lg-5">
+				
 				</div>
-				<div class="col-lg-8">
-					<form class="contact-form">
-						<div class="row">
-							<div class="col-md-6">
-								<input type="text" placeholder="Your Name">
-							</div>
-							<div class="col-md-6">
-								<input type="text" placeholder="Your E-mail">
-							</div>
-							<div class="col-md-12">
-								<input type="text" placeholder="Subject">
-								<textarea placeholder="Your Message"></textarea>
-								<button class="site-btn">send message</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-					</div>
-	</section>
-	<!-- Contact Section end -->
+    <div class="row contact-container">
+      <div class="col-lg-12">
+        <div class="card card-shadow border-0 mb-4">
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="contact-box p-4">
+                <h4 class="title">Contact Us</h4>
+                <form>
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="name">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="email">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="phone">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="location">
+                      </div>
+                    </div>
+                    <div class="col-lg-12">
+                      <div class="form-group mt-3">
+                        <input class="form-control" type="text" placeholder="message">
+                      </div>
+                    </div>
+                    <div class="col-lg-12">
+                      <button type="submit" class="btn btn-danger-gradiant mt-3 mb-3 text-white border-0 py-2 px-3"><span> SUBMIT NOW <i class="ti-arrow-right"></i></span></button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="col-lg-4 bg-image" style="background-image:url(https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/contact/1.jpg)">
+              <div class="detail-box p-4">
+                <h5 class="text-white font-weight-light mb-3">ADDRESS</h5>
+                <p class="text-white op-7">601 Sherwood Ave.
+                  <br> San Bernandino</p>
+                <h5 class="text-white font-weight-light mb-3 mt-4">CALL US</h5>
+                <p class="text-white op-7">251 546 9442
+                  <br> 630 446 8851</p>
+                <div class="round-social light">
+                  <a href="#" class="ml-0 text-decoration-none text-white border border-white rounded-circle"><i class="icon-social-facebook"></i></a>
+                  <a href="#" class="text-decoration-none text-white border border-white rounded-circle"><i class="icon-social-twitter"></i></a>
+                  <a href="#" class="text-decoration-none text-white border border-white rounded-circle"><i class="icon-social-youtube"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-	<!-- Score Section end -->
-	<!-- Score Section end -->
+	
+	<!-- Header Section end -->
 
-	<!-- Footer Section -->
-	<footer class="footer-section">
-		<div class="container">
-				</div>
-				<div class="col-lg-3 col-sm-6">
-					</div>
-				<div class="col-lg-3 col-sm-6">
-					
-	<!--====== Javascripts & Jquery ======-->
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.slicknav.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/jquery-ui.min.js"></script>
-	<script src="js/main.js"></script>
-
+	
 	</body>
 </html>
