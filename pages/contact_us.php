22d3c9ea85a685b1e745b8485484b97f0e3ee8fb
Gopika-gs <email@gsgopika00@gmail.com>
2020-09-25 11:42:18 +0530

    modified contact us page

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -1,70 +1,87 @@
-<div class="contact2">
-	<div class="contact2" style="background-image:url(images/55.jpg)" id="contact">
-  <div class="container">
-    <div class="row cotact-container">
-      <div class="col-lg-12">
-        <div class="card card-shadow border-0 mb-4" style="margin-top:100px;">
-          <div class="row">
-            <div class="col-lg-8">
-              <div class="contact-box p-4">
-                <h4 class="title">Contact Us</h4>
-                <form>
-                  <div class="row">
-                    <div class="col-lg-6">
-                      <div class="form-group mt-3">
-                        <input class="form-control" type="text" placeholder="name">
-                      </div>
-                    </div>
-                    <div class="col-lg-6">
-                      <div class="form-group mt-3">
-                        <input class="form-control" type="text" placeholder="email">
-                      </div>
-                    </div>
-                    <div class="col-lg-6">
-                      <div class="form-group mt-3">
-                        <input class="form-control" type="text" placeholder="phone">
-                      </div>
-                    </div>
-                    <div class="col-lg-6">
-                      <div class="form-group mt-3">
-                        <input class="form-control" type="text" placeholder="location">
-                      </div>
-                    </div>
-                    <div class="col-lg-12">
-                      <div class="form-group mt-3">
-                        <input class="form-control" type="text" placeholder="message">
-                      </div>
-                    </div>
-                    <div class="col-lg-12">
-                     <button class="site-btn" name="Submit" type="submit">submit </button>
-                    </div>
+<div class="contact3 py-5">
+  <div class="row no-gutters">
+    <div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+          <div class="card-shadow"  style="margin-top:100px;">
+            <img src="images/2.jpg"> 
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="contact-box ml-3" style="margin-top:100px;">
+            <h1 class="font-weight-light mt-2">Quick Contact</h1>
+            <form class="mt-4">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="form-group mt-2">
+                    <input class="form-control" type="text" placeholder="name">
                   </div>
-                </form>
+                </div>
+                <div class="col-lg-12">
+                  <div class="form-group mt-2">
+                    <input class="form-control" type="email" placeholder="email address">
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="form-group mt-2">
+                    <input class="form-control" type="text" placeholder="phone">
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="form-group mt-2">
+                    <textarea class="form-control" rows="3" placeholder="message"></textarea>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <button class="site-btn">Submit</button>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-4 bg-image" style="background-image:url(images/111.jpg)">
-              <div class="detail-box p-4">
-                <h5 class="text-white font-weight-light mb-3">ADDRESS</h5>
-                <p class="text-white op-7">601 Sherwood Ave.
-                  <br> San Bernandino</p>
-                <h5 class="text-white font-weight-light mb-3 mt-4">CALL US</h5>
-                <p class="text-white op-7">251 546 9442
-                  <br> 630 446 8851</p>
-                <div class="round-social light">
-                  <a href="#" class="ml-0 text-decoration-none text-white border border-white rounded-circle"><i class="fa fa-facebook-square"></i></a>
-                  <a href="#" class="text-decoration-none text-white border border-white rounded-circle"><i class="icon-social-twitter"></i></a>
-                  <a href="#" class="text-decoration-none text-white border border-white rounded-circle"><i class="icon-social-youtube"></i></a>
+            </form>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="card mt-4 border-0 mb-4">
+            <div class="row">
+              <div class="col-lg-4 col-md-4">
+                <div class="card-body d-flex align-items-center c-detail pl-0">
+                  <div class="mr-3 align-self-center">
+                    <img src="images/location.png">
+                  </div>
+                  <div class="">
+                    <h6 class="font-weight-medium">Address</h6>
+                    <p class="">601 Sherwood Ave.
+                      <br> San Bernandino</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4">
+                <div class="card-body d-flex align-items-center c-detail">
+                  <div class="mr-3 align-self-center">
+                    <img src="images/phone.png">
+                  </div>
+                  <div class="">
+                    <h6 class="font-weight-medium">Phone</h6>
+                    <p class="">251 546 9442
+                      <br> 630 446 8851</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4">
+                <div class="card-body d-flex align-items-center c-detail">
+                  <div class="mr-3 align-self-center">
+                    <img src="images/@.png"> 
+                  </div>
+                  <div class="">
+                    <h6 class="font-weight-medium">Email</h6>
+                    <p class="">
+                      complaintify20@gmail.com
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-		<br>
-		<br />
-		<br />
-		<br />
-		<br>
-		<br>
       </div>
     </div>
   </div>
